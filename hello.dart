f8a6751a45216c8e5285781fad59b1cbc79e96de
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-12 16:13:07 +0300

    new file:   hello.dart

---
+++ hello.dart
@@ -0,0 +1,14 @@
+void main(){
+int age = 25;
+double height = 1.78;
+String name = 'Hassan';
+bool active = true;
+
+var city = 'Nairobi';
+var score = 100;
+final String country = 'Uganda';
+const double tax_rate = 0.18;
+print("My name is $name and am $age yours old")
+
+
+}
