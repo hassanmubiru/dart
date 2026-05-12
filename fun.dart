e6289b6ead7553a60a018ab693091b1d5ebdbf42
hassanmubiru <hassanmubiru7@gmail.com>
2026-05-12 09:10:05 +0300

    modified:   fun.dart

--- fun.dart
+++ fun.dart
@@ -1,11 +1,11 @@
-// return_type function_name(parameters){
-//     // body of function
-//     return value;
+// // return_type function_name(parameters){
+// //     // body of function
+// //     return value;
+// // }
+
+
+// // function_name(argument_list)
+
+// returntype functionName(parameter1,parameter2,...){
+//     print("Function")
 // }
-
-
-// function_name(argument_list)
-
-returntype functionName(parameter1,parameter2,...){
-    print("Function")
-}
